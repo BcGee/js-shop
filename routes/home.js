c7644ee77eb25bd772aac5f1a45d2164e4bc2e2f
YongTae Lee <blancspace@naver.com>
2021-05-31 22:37:42 +0900

    images added

--- routes/home.js
+++ routes/home.js
@@ -3,15 +3,22 @@ var router = express.Router();
 var ProductsModel = require('../models/ProductsModel');
 var BannersModel = require('../models/BannersModel');
 
+
+
+
+
 /* GET home page. */
 router.get('/', function(req,res){
 
 
+
+    
+    
     BannersModel.find( function(err,banners){ //첫번째 인자는 err, 두번째는 받을 변수명
         ProductsModel.find( function(err,products){ //첫번째 인자는 err, 두번째는 받을 변수명
 
             res.render( 'home' , 
-                { products : products,
+                {  products : products,
                     banners: banners 
                 } // DB에서 받은 products를 products변수명으로 내보냄
             );
