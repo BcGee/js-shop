91c5bdee583a11f1fb4ece88b10c2ce821fbd835
YongTae Lee <blancspace@naver.com>
2021-05-31 23:20:50 +0900

    Category divided

--- routes/home.js
+++ routes/home.js
@@ -6,25 +6,87 @@ var BannersModel = require('../models/BannersModel');
 
 
 
+ProductsModel.find({category : 1},function(err,products){
+
+    
+});
 
 /* GET home page. */
 router.get('/', function(req,res){
 
 
+    
 
     
     
     BannersModel.find( function(err,banners){ //첫번째 인자는 err, 두번째는 받을 변수명
-        ProductsModel.find( function(err,products){ //첫번째 인자는 err, 두번째는 받을 변수명
+        ProductsModel.find(  function(err,products){ //첫번째 인자는 err, 두번째는 받을 변수명
+
+
 
             res.render( 'home' , 
                 {  products : products,
                     banners: banners 
+                    
                 } // DB에서 받은 products를 products변수명으로 내보냄
             );
         });
     });
 
 });
+
+
+router.get('/red', function(req,res){
+    
+    BannersModel.find( function(err,banners){ //첫번째 인자는 err, 두번째는 받을 변수명
+        ProductsModel.find({category : 1},function(err,products){
+
+            res.render( 'home' , 
+                {  products : products,
+                    banners: banners 
+                    
+                } // DB에서 받은 products를 products변수명으로 내보냄
+            );
+        });
+    });
+
+});
+
+
+router.get('/green', function(req,res){
+    
+    BannersModel.find( function(err,banners){ //첫번째 인자는 err, 두번째는 받을 변수명
+        ProductsModel.find({category : 2},function(err,products){
+
+            res.render( 'home' , 
+                {  products : products,
+                    banners: banners 
+                    
+                } // DB에서 받은 products를 products변수명으로 내보냄
+            );
+        });
+    });
+
+});
+
+
+router.get('/blue', function(req,res){
+    
+    BannersModel.find( function(err,banners){ //첫번째 인자는 err, 두번째는 받을 변수명
+        ProductsModel.find({category : 3},function(err,products){
+
+            res.render( 'home' , 
+                {  products : products,
+                    banners: banners 
+                    
+                } // DB에서 받은 products를 products변수명으로 내보냄
+            );
+        });
+    });
+
+});
+
+
+
 
 module.exports = router;
